f3ef5f76ae560821e7ca3368b4103178be047a57
RoelVillaluz <roelo.villaluz@gmail.com>
2025-06-17 13:08:00 +0800

    fixed mistake: accidentally added forms to admin.py

--- dti_project/users/admin.py
+++ dti_project/users/admin.py
@@ -1,19 +1,3 @@
-from django import forms
 from django.contrib import admin
-from django.contrib.auth.forms import UserCreationForm
-from .models import User
 
 # Register your models here.
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-        return user
