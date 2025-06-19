5f1ddee085a5d097aed0149f753cfb5b431f501f
RoelVillaluz <roelo.villaluz@gmail.com>
2025-06-19 15:19:38 +0800

    added custom admin for User model

--- dti_project/users/admin.py
+++ dti_project/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'is_staff')
+    list_filter = ('is_staff', )
+    fields = ('first_name', 'last_name', 'email', 'is_active', 'is_staff')
+    search_fields = ('first_name', 'last_name', 'email')
