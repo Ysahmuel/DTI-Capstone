8940e68505df447a5a4d5bcbe294e65e41101ced
RoelVillaluz <roelo.villaluz@gmail.com>
2025-06-19 15:23:32 +0800

    added login.html (no functionality yet)

--- dti_project/users/views.py
+++ dti_project/users/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login, logout as auth_logout
 
 # Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'users/login.html'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self) -> str:
+        return reverse_lazy('dashboard')
+    
+    
 class CustomRegisterView(CreateView):
     template_name = 'users/register.html'
     
