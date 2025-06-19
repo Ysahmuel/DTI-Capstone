e34dc5b5ebc2e7e475fce22cc97330f628062606
RoelVillaluz <roelo.villaluz@gmail.com>
2025-06-19 15:23:53 +0800

    added form_valid and success url to customRegisterView

--- dti_project/users/views.py
+++ dti_project/users/views.py
@@ -16,6 +16,19 @@ class CustomLoginView(LoginView):
     
 class CustomRegisterView(CreateView):
     template_name = 'users/register.html'
+    redirect_authenticated_user = True
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
+        print("Form is valid, saving user now...")
+        response = super().form_valid(form)
+        user = form.save()
+
+        print(f"User created: {user}")
+        login(self.request, user)
+        
+        return response
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
