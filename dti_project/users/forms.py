6330fbf45a1ac0805d1bc7d5f760e23556f0b11c
RoelVillaluz <roelo.villaluz@gmail.com>
2025-06-21 09:24:29 +0800

    added customLoginForm

--- dti_project/users/forms.py
+++ dti_project/users/forms.py
@@ -1,7 +1,26 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
+from django.contrib.auth import authenticate
+from django.utils.translation import gettext_lazy as _
 
+class CustomLoginForm(AuthenticationForm):
+    username = forms.EmailField(label="Email")  # override 'username' field to be an EmailField
+
+    def clean(self):
+        email = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if email and password:
+            self.user_cache = authenticate(self.request, username=email, password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError(_('Invalid email or password'))
+        return self.cleaned_data
+
+
+    def get_user(self):
+        return getattr(self, 'user_cache', None)
 
 class CustomUserCreationForm(UserCreationForm):
     first_name = forms.CharField(required=True)
