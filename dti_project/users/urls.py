caf656e657d3dbb91a386386e517e60b118ba4c4
RoelVillaluz <roelo.villaluz@gmail.com>
2025-06-21 10:19:45 +0800

    added htmx for displaying warning message if email already exists

--- dti_project/users/urls.py
+++ dti_project/users/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path('sign-in', views.CustomLoginView.as_view(), name="sign-in"),
     path('register', views.CustomRegisterView.as_view(), name="register"),
-    path('logout', views.logout, name="logout")
+    path('logout', views.logout, name="logout"),
 ]
+
+htmxpatterns = [
+    path('check_email_exists', views.check_email_exists, name="check-email-exists"),
+]
+
+urlpatterns += htmxpatterns
--- dti_project/users/views.py
+++ dti_project/users/views.py
@@ -1,10 +1,10 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView, LogoutView
 from .forms import CustomLoginForm, CustomUserCreationForm
-from django.contrib.auth import login, logout as auth_logout
+from django.contrib.auth import login, logout as auth_logout, get_user_model
 
 # Create your views here.
 class CustomLoginView(LoginView):
@@ -40,3 +40,13 @@ class CustomRegisterView(CreateView):
 def logout(request):
     auth_logout(request)
     return HttpResponseRedirect("/")
+
+def check_email_exists(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        if email:
+            if get_user_model().objects.filter(email=email).exists():
+                return HttpResponse('<span style="color: red;">This email already exists</span>')
+            else:
+                return HttpResponse('<span style="color: green;">Email is available</span>')
+    return HttpResponse("")
