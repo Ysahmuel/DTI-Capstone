9ee3fe50b7f537a1e9a765a5e43657a0033dcccb
RoelVillaluz <roelo.villaluz@gmail.com>
2025-06-21 08:56:51 +0800

    ded urlpattern for customLoginView

--- dti_project/users/urls.py
+++ dti_project/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('sign-in', views.CustomLoginView.as_view(), name="sign-in"),
     path('register', views.CustomRegisterView.as_view(), name="register"),
     path('logout', views.logout, name="logout")
 ]
