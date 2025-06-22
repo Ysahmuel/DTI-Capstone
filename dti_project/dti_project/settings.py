31853a6f873cbc2154a27bd6fdc389744842cc5a
RoelVillaluz <roelo.villaluz@gmail.com>
2025-06-22 15:57:33 +0800

    updated dashboardView to redirect to login page if user is not authenticated

--- dti_project/dti_project/settings.py
+++ dti_project/dti_project/settings.py
@@ -135,7 +135,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = '/login/'
+LOGIN_URL = 'users/sign-in'
 
 AUTH_USER_MODEL = 'users.User'
 
