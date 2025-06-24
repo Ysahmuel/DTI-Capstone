51714d36759ece672232d25916ad4273250eb0f9
RoelVillaluz <roelo.villaluz@gmail.com>
2025-06-24 15:11:41 +0800

    added form_valid, invalid, and messages to CreateSalesPromotionView

--- dti_project/documents/views.py
+++ dti_project/documents/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from .forms import ProductCoveredFormSet, SalesPromotionPermitApplicationForm
 from .models import ProductCovered, SalesPromotionPermitApplication
+from django.contrib import messages
+from django.db import transaction
 
 # Create your views here.
 class CreateSalesPromotionView(CreateView):
@@ -10,9 +12,6 @@ class CreateSalesPromotionView(CreateView):
     context_object_name = 'sales_promo'
     form_class = SalesPromotionPermitApplicationForm
 
-    def post(self, request, *args, **kwargs):
-        pass
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if self.request.POST:
@@ -20,3 +19,36 @@ class CreateSalesPromotionView(CreateView):
         else:
             context['product_formset'] = ProductCoveredFormSet(queryset=ProductCovered.objects.none())
         return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        product_formset = context['product_formset']
+
+        # Use transaction to ensure data integrity
+        with transaction.atomic():
+            # Save the main form first
+            self.object = form.save()
+
+            if product_formset.is_valid():
+                # Save formset instances with the relationship
+                instances = product_formset.save(commit=False)
+                for instance in instances:
+                    # Assuming ProductCovered has a ForeignKey to SalesPromotionPermitApplication
+                    instance.sales_promotion = self.object
+                    instance.save()
+
+                # Handle any deletions
+                product_formset.save_m2m()
+
+                messages.success(
+                    self.request,
+                    'Sales Promotion Permit Application created successfully!'
+                )
+                return super().form_valid(form)
+            else:
+                # If formset is invalid, return to form with errors
+                return self.form_invalid(form)
+            
+    def form_invalid(self, form):
+        messages.error(self.request, 'Please correct the errors below.')
+        return super().form_invalid()
