c62736a4139528743d0b74358f992515a87de357
RoelVillaluz <roelo.villaluz@gmail.com>
2025-07-01 16:32:54 +0800

    added sales_promotion_detail.html

--- dti_project/documents/urls.py
+++ dti_project/documents/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("create/sales-promotion", views.CreateSalesPromotionView.as_view(), name="create-sales-promotion"),
-
+    path("sales-promotion-application/<int:pk>", views.SalesPromotionDetailView.as_view(), name="sales-promotion-application"),
 ]
