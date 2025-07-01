e7d95fb957b3991166fd5bc119b4ec8101107f60
RoelVillaluz <roelo.villaluz@gmail.com>
2025-07-02 07:52:37 +0800

    added custom admin for ProductCovered model

--- dti_project/documents/admin.py
+++ dti_project/documents/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SalesPromotionPermitApplication
+from .models import ProductCovered, SalesPromotionPermitApplication
 
 # Register your models here.
 @admin.register(SalesPromotionPermitApplication)
@@ -7,3 +7,11 @@ class SalesPromotionAdmin(admin.ModelAdmin):
     list_display = ('promo_title', )
     search_fields = ('promo_title', 'sponsor_name', 'advertising_agency_name')
     
+@admin.register(ProductCovered)
+class ProductCoveredAdmin(admin.ModelAdmin):
+    list_display = ('name', 'permit_application_title')
+    search_fields = ('name', 'permit_application__promo_title')
+
+    def permit_application_title(self, obj):
+        return obj.permit_application.promo_title
+    permit_application_title.short_description = 'Promo Title'
