8e68621d57efabd18436638b4be5abebd11128d4
RoelVillaluz <roelo.villaluz@gmail.com>
2025-07-11 18:32:24 +0800

    added profile_pciture field to User

--- dti_project/users/admin.py
+++ dti_project/users/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
-# Register your models here.
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = ('first_name', 'last_name', 'email', 'is_staff')
     list_filter = ('is_staff', )
-    fields = ('first_name', 'last_name', 'email', 'is_active', 'is_staff')
     search_fields = ('first_name', 'last_name', 'email')
+
+    # Extend the default fieldsets to include profile_picture
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Profile', {
+            'fields': ('profile_picture',),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
--- dti_project/users/models.py
+++ dti_project/users/models.py
@@ -3,4 +3,8 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    profile_picture = models.ImageField(
+        upload_to='profile_pictures/',
+        default='profile_pictures/default-avatar-icon.jpg',
+        blank=True
+    )
