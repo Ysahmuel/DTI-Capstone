daed2214aa814cb4e8e49aa87ec1d3ceeac76c7c
RoelVillaluz <roelo.villaluz@gmail.com>
2025-07-17 15:05:55 +0800

    added init_py to views

---
+++ dti_project/documents/views/__init__.py
@@ -0,0 +1,9 @@
+from .create_views import (
+    CreateSalesPromotionView,
+    CreatePersonalDataSheetView,
+    CreateServiceRepairAccreditationApplication,
+)
+from .detail_views import (
+    SalesPromotionDetailView,
+    PersonalDataSheetDetailView,
+)
