462a081f8c5bd5c95d13c242b4a64572e6cc2330
RoelVillaluz <roelo.villaluz@gmail.com>
2025-07-17 08:50:51 +0800

    added is_textarea custom fitler

--- dti_project/documents/templatetags/custom_filters.py
+++ dti_project/documents/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from django.forms.widgets import Textarea
 
 register = template.Library()
 
@@ -10,3 +11,7 @@ def titlecase(value):
 def get_form_field(form, field_name):
     """Safely get a field from a Django form by name"""
     return form[field_name]
+
+@register.filter
+def is_textarea(field):
+    return isinstance(field.field.widget, Textarea)
