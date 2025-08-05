9f4b5209885804198135eb8758c70cab965bff8d
RoelVillaluz <roelo.villaluz@gmail.com>
2025-08-05 09:44:56 +0800

    changed order of payment remark choices

--- dti_project/documents/utils/model_helpers.py
+++ dti_project/documents/utils/model_helpers.py
@@ -1,9 +1,9 @@
 from django.db import models  
-from documents.model_choices import REMARKS_CHOICES
+from documents.model_choices import PERMIT_FEE_REMARK_CHOICES
 
 
 def remark_amount_fields(prefix):
     return {
-        f"{prefix}_remark": models.CharField(max_length=255, blank=True, null=True, choices=REMARKS_CHOICES),
+        f"{prefix}_remark": models.CharField(max_length=255, blank=True, null=True, choices=PERMIT_FEE_REMARK_CHOICES),
         f"{prefix}_amount": models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True, help_text=f"{prefix} amount in ₱"),
     }
