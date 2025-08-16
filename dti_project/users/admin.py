9fd056e65f9091dbfb87825b77707a75869c7d1f
RoelVillaluz <roelo.villaluz@gmail.com>
2025-08-16 08:37:30 +0800

    added role to User model

--- dti_project/users/admin.py
+++ dti_project/users/admin.py
@@ -1,17 +1,36 @@
+# admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
 class UserAdmin(BaseUserAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'is_staff')
-    list_filter = ('is_staff', )
+    list_display = ('first_name', 'last_name', 'role', 'email', 'is_staff')
+    list_filter = ('is_staff', 'role')
     search_fields = ('first_name', 'last_name', 'email')
 
-    # Extend the default fieldsets to include profile_picture
+    # Extend the default fieldsets to include role + profile_picture
     fieldsets = BaseUserAdmin.fieldsets + (
         ('Profile', {
-            'fields': ('profile_picture',),
+            'fields': ('role', 'profile_picture'),
         }),
     )
+
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('role', 'profile_picture'),
+        }),
+    )
+
+    def save_model(self, request, obj, form, change):
+        # If user is created as superuser → force role to "admin"
+        if obj.is_superuser:
+            obj.role = 'admin'
+
+        # If role is admin → make sure they have staff access
+        if obj.role == "admin":
+            obj.is_staff = True
+
+        super().save_model(request, obj, form, change)
 
 admin.site.register(User, UserAdmin)
--- dti_project/users/models.py
+++ dti_project/users/models.py
@@ -3,10 +3,19 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 
 # Create your models here.
 class User(AbstractUser):
+    class Roles(models.TextChoices):
+        BUSINESS_OWNER = "business_owner", "Business Owner"
+        ADMIN = "admin", "Admin"
+
     profile_picture = models.ImageField(
         upload_to='profile_pictures/',
         default='profile_pictures/default-avatar-icon.jpg',
         blank=True
+    )
+    role = models.CharField(
+        max_length=20,
+        choices=Roles.choices,
+        default=Roles.BUSINESS_OWNER
     )
 
     def __str__(self):
