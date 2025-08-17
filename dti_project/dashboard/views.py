4cd7e8273a8092957a13d1db7ee7a464f409ba59
RoelVillaluz <roelo.villaluz@gmail.com>
2025-08-17 09:33:42 +0800

    updated dashboardView to fetch only needed fields and not all data of all objects

--- dti_project/dashboard/views.py
+++ dti_project/dashboard/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from documents.models import ChecklistEvaluationSheet, InspectionValidationReport, OrderOfPayment, PersonalDataSheet, SalesPromotionPermitApplication, ServiceRepairAccreditationApplication
-
+from django.db.models import Value
+from django.db.models.functions import Concat
+    
 # Create your views here.
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "dashboard/dashboard.html"
 
     @staticmethod
     def get_queryset_or_all(model, user):
-        if user.role == 'admin':
-            return model.objects.all()
-        return model.objects.filter(user=user)
+        qs = model.objects.all() if user.role == 'admin' else model.objects.filter(user=user)
+        
+        return qs.only('pk', 'id')  # Add other fields that __str__ methods need
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
