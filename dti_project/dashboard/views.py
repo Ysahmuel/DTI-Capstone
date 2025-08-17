f950fea5f539c51cd678df258625e33035226b57
RoelVillaluz <roelo.villaluz@gmail.com>
2025-08-17 09:18:35 +0800

    updated dashboard to show data depending on user role (only shows forms created by user if business owner)

--- dti_project/dashboard/views.py
+++ dti_project/dashboard/views.py
@@ -7,16 +7,23 @@ from documents.models import ChecklistEvaluationSheet, InspectionValidationRepor
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "dashboard/dashboard.html"
 
+    @staticmethod
+    def get_queryset_or_all(model, user):
+        if user.role == 'admin':
+            return model.objects.all()
+        return model.objects.filter(user=user)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        user = self.request.user
 
         context.update({
-            'sales_promos': SalesPromotionPermitApplication.objects.all(),
-            'personal_data_sheets': PersonalDataSheet.objects.all(),
-            'service_accreditations': ServiceRepairAccreditationApplication.objects.all(),
-            'inspection_reports': InspectionValidationReport.objects.all(),
-            'orders_of_payment': OrderOfPayment.objects.all(),
-            'checklist_evaluation_sheets': ChecklistEvaluationSheet.objects.all()
+            'sales_promos': self.get_queryset_or_all(SalesPromotionPermitApplication, user),
+            'personal_data_sheets': self.get_queryset_or_all(PersonalDataSheet, user),
+            'service_accreditations': self.get_queryset_or_all(ServiceRepairAccreditationApplication, user),
+            'inspection_reports': self.get_queryset_or_all(InspectionValidationReport, user),
+            'orders_of_payment': self.get_queryset_or_all(OrderOfPayment, user),
+            'checklist_evaluation_sheets': self.get_queryset_or_all(ChecklistEvaluationSheet, user),
         })
 
         return context
