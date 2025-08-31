93dbb0b69fbee1704cb265adb8eaaa8e42116637
RoelVillaluz <roelo.villaluz@gmail.com>
2025-08-31 15:00:46 +0800

    added documents_list template and view

--- dti_project/documents/views/list_views.py
+++ dti_project/documents/views/list_views.py
@@ -1,7 +1,28 @@
 from django.views.generic import ListView
+from ..mixins.permissions_mixins import UserRoleMixin
+from ..models import ChecklistEvaluationSheet, InspectionValidationReport, OrderOfPayment, PersonalDataSheet, SalesPromotionPermitApplication, ServiceRepairAccreditationApplication
 
-class AllDocumentListView(ListView):
-    pass
+class AllDocumentListView(UserRoleMixin, ListView):
+    template_name = 'documents/list_templates/all_documents_list.html'
+    context_object_name = 'documents'
+
+    def get_queryset(self):
+        return []
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        context.update({
+            'sales_promos': self.get_queryset_or_all(SalesPromotionPermitApplication, user),
+            'personal_data_sheets': self.get_queryset_or_all(PersonalDataSheet, user),
+            'service_accreditations': self.get_queryset_or_all(ServiceRepairAccreditationApplication, user),
+            'inspection_reports': self.get_queryset_or_all(InspectionValidationReport, user),
+            'orders_of_payment': self.get_queryset_or_all(OrderOfPayment, user),
+            'checklist_evaluation_sheets': self.get_queryset_or_all(ChecklistEvaluationSheet, user),
+        })
+
+        return context
 
 class SalesPromotionListView(ListView):
     pass
