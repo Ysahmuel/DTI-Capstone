a7a6544ac43b761ce384550a012f62645a6c9025
RoelVillaluz <roelo.villaluz@gmail.com>
2025-09-13 08:47:02 +0800

    added notifications.js + css

---
+++ dti_project/dashboard/static/dashboard/js/notifications.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const bellIcon = document.querySelector('.header-notifications .fa-bell');
+    const notificationsList = document.querySelector('.notifications-list');
+    const suggestionsBox = document.getElementById('suggestions-box');
+
+    if (bellIcon && notificationsList) {
+        bellIcon.addEventListener('click', () => {
+            notificationsList.classList.toggle('visible');
+
+            if (suggestionsBox.classList.contains('visible')) {
+                suggestionsBox.classList.remove('visible')
+            }
+        });
+    }
+
+    // automatically hide notifications list if search suggestion box is visible
+    if (suggestionsBox) {
+        const observer = new MutationObserver(() => {
+            if (suggestionsBox.style.visibility === 'visible') {
+                notificationsList.classList.remove('visible');
+            }
+        });
+
+        observer.observe(suggestionsBox, { attributes: true, attributeFilter: ['style'] });
+    }
+
+    // click outside -> remove visibility
+    document.addEventListener('click', function(e) {
+        if (!bellIcon.contains(e.target) && !notificationsList.contains(e.target)) {
+            notificationsList.classList.remove('visible');
+        }
+    });
+
+})
