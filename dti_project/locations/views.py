2ec7fc18814e60ed9fecf87047b999c844949b85
RoelVillaluz <roelo.villaluz@gmail.com>
2025-10-16 16:09:08 +0800

    updated location api views to get only values for faster performance

--- dti_project/locations/views.py
+++ dti_project/locations/views.py
@@ -7,30 +7,27 @@ from .serializers import RegionSerializer, ProvinceSerializer, CityMunicipalityS
 @api_view(['GET'])
 def get_regions(request):
     """Return all regions."""
-    regions = Region.objects.all().order_by('name')
-    serializer = RegionSerializer(regions, many=True)
-    return Response(serializer.data)
+    regions = Region.objects.all().order_by('name').values('id', 'name', 'code')  # Use values() for speed
+    return Response(list(regions))
 
 
 @api_view(['GET'])
 def get_provinces(request, region_id):
     """Return all provinces within a region."""
-    provinces = Province.objects.filter(region_id=region_id).order_by('name')
-    serializer = ProvinceSerializer(provinces, many=True)
-    return Response(serializer.data)
+    provinces = Province.objects.filter(region_id=region_id).order_by('name').values('id', 'name', 'code')
+    return Response(list(provinces))
 
 
 @api_view(['GET'])
 def get_cities(request, province_id):
     """Return all cities or municipalities within a province."""
-    cities = CityMunicipality.objects.filter(province_id=province_id).order_by('name')
-    serializer = CityMunicipalitySerializer(cities, many=True)
-    return Response(serializer.data)
+    cities = CityMunicipality.objects.filter(province_id=province_id).order_by('name').values('id', 'name', 'code')
+    return Response(list(cities))
 
 
 @api_view(['GET'])
 def get_barangays(request, city_id):
     """Return all barangays within a city or municipality."""
-    barangays = Barangay.objects.filter(city_id=city_id).order_by('name')
-    serializer = BarangaySerializer(barangays, many=True)
-    return Response(serializer.data)
+    # This is likely the slowest - barangays can be hundreds per city
+    barangays = Barangay.objects.filter(city_id=city_id).order_by('name').values('id', 'name', 'code')
+    return Response(list(barangays))
